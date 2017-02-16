d9cd9f28b43eb18bd0a789b93c6275ad4b3166b6
TylerOlli <ollityler@gmail.com>
2017-02-16 17:43:40 -0500

    Added MovieDB / Switch Statement

--- liri.js
+++ liri.js
@@ -1,20 +1,28 @@
+var cmd = process.argv[2];
+var cmd2 = process.argv[3];
+
+var keys = require('./keys.js');
 var Twitter = require('twitter');
- 
+var spotify = require('spotify');
+var mdb = require('moviedb')('c067d2c803a661fea4a55c14a7f1beee');
+var fs = require('fs');
+
 var client = new Twitter({
-  consumer_key: 'u8pkAh9dWmvf5MdR0ORNMVlw4',
-  consumer_secret: 'wf9cfG3B16mAdxCf2xHKs3RRQpScIDzYldeXkfwiTkLjubIi7n',
-  access_token_key: '832061185235283968-OgiRBGkSramzZLxUtVCmbATr7lrOQrf',
-  access_token_secret: '909a5LpQo9cEsL8lla4vWvgQnFI7iIdqf2PAqcJkeeyfZ',
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
 });
+
  
 var params = {screen_name: 'fullstack_ninja'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
+    writeLog(tweets);
   }
 });
 
-var spotify = require('spotify');
  
 spotify.search({ type: 'track', query: 'I Want it That Way' }, function(err, data) {
     if ( err ) {
@@ -22,5 +30,64 @@ spotify.search({ type: 'track', query: 'I Want it That Way' }, function(err, dat
         return;
     }
  
-    console.log(data); 
+    console.log(data);
+    writeLog(data); 
 });
+
+mdb.searchMovie({query: 'Mr. Nobody' }, function(err, res){
+  console.log(res);
+  writeLog(res); 
+});
+
+
+function writeLog(data){
+	fs.appendFile('log.txt', data + '\n', 'UTF-8', function(error){
+		if (error) throw error;
+	})
+}
+
+switch(cmd) {
+
+	case 'my-tweets':
+		getTwitter();
+		writeLog('my-tweets');
+	break;
+
+	case 'spotify-this-song':
+		getSpotify(cmd2);
+		writeLog('spotify-this-song');
+	break;
+
+	case 'movie-this':
+		getMovies(cmd2);
+		writeLog('movie-this');
+	break;
+
+	case 'do-what-it-says':
+		fs.readFile("random.txt", "UTF-8", function(error, data){
+			if (error) throw error;
+			data = data.split(',');
+
+			if(data[0] === 'my-tweets'){
+				getTwitter();
+				writeLog('my-tweets');
+			}else if(data[0] === 'spotify-this-song'){
+				getSpotify(data[1]);
+				writeLog('spotify-this-song');
+			}else if(data[0] === 'movie-this'){
+				getMovies(data[1]);
+				writeLog('movie-this');
+			}
+		})
+	break;
+
+	default:
+		console.log('');
+		console.log("You didn\'t input a valid command");
+		console.log('');
+
+		writeLog('');
+		writeLog("You didn\'t input a valid command");
+		writeLog('');
+
+}
